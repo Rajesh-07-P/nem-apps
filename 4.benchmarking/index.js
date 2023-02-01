6900f6788cbece8bbcc5fa7209e239afd4c7ac8d
Rajesh-P-07 <rajesh.pachika@gmail.com>
2023-02-01 09:33:31 +0530

    updated benchmark

--- 4.benchmarking/index.js
+++ 4.benchmarking/index.js
@@ -4,13 +4,9 @@ const fs=require("fs");
 
 const server=http.createServer((req,res)=>{
     if(req.url=="/textsync"){
-        fs.readFileSync("./data.txt",{encoding:"utf-8"},(err,data)=>{
-            if(err){
-                res.end(err);
-            }else{
-                res.end(data);
-            }
-        })
+        let data=fs.readFileSync("./data.txt",{encoding:"utf-8"});
+
+        res.end(data);
     }
 
     if(req.url=="/textasync"){
@@ -23,7 +19,16 @@ const server=http.createServer((req,res)=>{
         })
     }
     if(req.url=="/textstream"){
-        
+        const dataStream=fs.createReadStream("./data.txt","utf-8");
+
+        dataStream.pipe(res);
+    }
+    if(req.url=="/textpromise"){
+        async function read(){
+            const data=await fs.promises.readFile("./data.txt","utf-8");
+            res.end(data);
+        }
+        read();
     }
 })
 

e33cae966c62f29787ff1bc2e2b841b52bcc5400
Rajesh-P-07 <rajesh.pachika@gmail.com>
2023-02-01 08:54:06 +0530

    updated file serveerver

--- 3.file_server/index.js
+++ 3.file_server/index.js
@@ -1,46 +1,59 @@
-const fs= require("fs");
+const fs = require("fs");
 
-const http=require("http");
+const http = require("http");
 
-const server=http.createServer((req,res)=>{
-    if(req.url=="/"){
-        fs.readdir(".",(err,files)=>{
-            if(err){
-                res.end("cannot be read");
-            }else{
-                files.forEach((file)=>{
-                    res.write(`<p>${file}</p>`);
-                })
-                res.end();
-            }
-        })
-    }
-    if(req.url=="/public"){
-        fs.readdir(`.${req.url}`,(err,files)=>{
-            if(err){
-                res.end("cannot be read");
-            }else{
-                files.forEach((file)=>{
-                    res.write(`<p>${file}</p>`);
-                })
-                res.end();
-            }
-        })
-    }
-    if(req.url=="/public/other"){
-        fs.readdir(`.${req.url}`,(err,files)=>{
-            if(err){
-                res.end("cannot be read");
-            }else{
-                files.forEach((file)=>{
-                    res.write(`<p>${file}</p>`);
-                })
-                res.end();
-            }
-        })
-    }
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    fs.readdir(".", (err, files) => {
+      console.log(files);
+      if (err) {
+        res.end("cannot be read");
+      } else {
+        let data = "";
+        files.forEach((file) => {
+          data += `<li><a href="http://localhost:4500/${file}">${file}</a></li>`;
+        });
+        res.setHeader("Content-type", "text/html");
+        res.end(`<ul style="margin:100px;line-height:40px">${data}</ul>`);
+      }
+    });
+  }
+  if (req.url) {
+    fs.readdir(`.${req.url}`, (err, files) => {
+      if (err) {
+        fs.readFile(`.${req.url}`, (err, data) => {
+          if (err) {
+            res.write(req.url);
+            res.end("cannot read file");
+          } else {
+            res.end(data);
+          }
+        });
+        // res.end("cannot be read");
+      } else {
+        let data="";
+        files.forEach((file) => {
+            data+=`<li>${file}</li>`;
+        });
+        res.setHeader("Content-type","text/html");
+        res.end(`<ul style="margin:100px;line-height:40px">${data}</ul>`);
+      }
+    });
+  }
+  if (req.url == "/public/other") {
+    fs.readdir(`.${req.url}`, (err, files) => {
+      if (err) {
+        res.end("cannot be read");
+      } else {
+        files.forEach((file) => {
+          res.write(`<p>${file}</p>`);
+        });
+        res.end();
+      }
+    });
+  }
 });
 
-server.listen(4500,()=>{
-    console.log("server started in port 4500");
-})
+server.listen(4500, () => {
+  console.log("server started in port 4500");
+});

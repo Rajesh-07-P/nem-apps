2d979ffb6d82cc827ed41adbd2978c65497241ca
Rajesh-P-07 <rajesh.pachika@gmail.com>
2023-02-02 10:05:06 +0530

    updated file server

--- 3.file_server/index.js
+++ 3.file_server/index.js
@@ -3,22 +3,8 @@ const fs = require("fs");
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  if (req.url == "/") {
-    fs.readdir(".", (err, files) => {
-      console.log(files);
-      if (err) {
-        res.end("cannot be read");
-      } else {
-        let data = "";
-        files.forEach((file) => {
-          data += `<li><a href="http://localhost:4500/${file}">${file}</a></li>`;
-        });
-        res.setHeader("Content-type", "text/html");
-        res.end(`<ul style="margin:100px;line-height:40px">${data}</ul>`);
-      }
-    });
-  }
   if (req.url) {
+    console.log(req.url);
     fs.readdir(`.${req.url}`, (err, files) => {
       if (err) {
         fs.readFile(`.${req.url}`, (err, data) => {
@@ -29,26 +15,13 @@ const server = http.createServer((req, res) => {
             res.end(data);
           }
         });
-        // res.end("cannot be read");
       } else {
         let data="";
         files.forEach((file) => {
-            data+=`<li>${file}</li>`;
+            data += `<li><a href="http://localhost:4500${req.url}/${file}">${file}</a></li>`;
         });
         res.setHeader("Content-type","text/html");
         res.end(`<ul style="margin:100px;line-height:40px">${data}</ul>`);
-      }
-    });
-  }
-  if (req.url == "/public/other") {
-    fs.readdir(`.${req.url}`, (err, files) => {
-      if (err) {
-        res.end("cannot be read");
-      } else {
-        files.forEach((file) => {
-          res.write(`<p>${file}</p>`);
-        });
-        res.end();
       }
     });
   }

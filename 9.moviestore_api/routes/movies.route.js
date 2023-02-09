e4c67f623a69dfcf21de0a1282d7f6f80dac7a8a
Rajesh-P-07 <rajesh.pachika@gmail.com>
2023-02-09 15:16:18 +0530

    updated moviestore api

--- 9.moviestore_api/routes/movies.route.js
+++ 9.moviestore_api/routes/movies.route.js
@@ -4,14 +4,14 @@ const { Moviemodel } = require("../db");
 const moviesRouter = express.Router();
 
 
-//samplerequest --->   localhost:3500/movies/?releaseYear=2014&sort=rating&order=dsc&limit=3  
+//samplerequest --->   localhost:3500/movies/?releaseYear=2014&sort=rating&order=dsc&limit=3&page=1  
 moviesRouter.get("/", async (req, res) => {
   try {
       const obj = {
         [req.query.sort]: req.query.order == "asc" ? 1 : -1,
       };
     console.log(obj);
-    let data = await Moviemodel.find(req.query).sort(obj).limit(req.query.limit);
+    let data = await Moviemodel.find(req.query).sort(obj).skip(req.query.limit*(req.query.page-1)).limit(req.query.limit);
     res.send(data);
   } catch (err) {
     console.log(err);
@@ -32,10 +32,34 @@ moviesRouter.get("/search", async (req, res) => {
 });
 
 moviesRouter.post("/post", (req, res) => {
-  Moviemodel.insertMany(req.body).then(() => {
+  Moviemodel.insertMany(req.body).then((data) => {
     console.log("added to db");
+    console.log(data);
   });
   res.end("posted");
 });
+
+
+moviesRouter.patch("/editmovie/:id",async(req,res)=>{
+  let id=req.params.id;
+  let payload=req.body;
+  try{
+    Moviemodel.findByIdAndUpdate({_id:id},payload);
+    res.send("updated successfully");
+  }catch(err){
+    console.log(err);
+    res.send("Something went wrong");
+  }
+})
+moviesRouter.delete("/editmovie/:id",async(req,res)=>{
+  let id=req.params.id;
+  try{
+    Moviemodel.findByIdAndDelete({_id:id});
+    res.send("deleted successfully");
+  }catch(err){
+    console.log(err);
+    res.send("Something went wrong");
+  }
+})
 
 module.exports = { moviesRouter };
